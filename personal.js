c8e95005965a4d07356fd0df2538e44ebbd3ca0e
Shabir1615 <shabirjaan.shafi@gmail.com5>
2023-04-20 12:09:04 +0530

    edited

--- personal.js
+++ personal.js
@@ -1,104 +1,78 @@
-// $(document).ready(function() {
-//   $('#contact-form').submit(function(event) {
-//     event.preventDefault(); // prevent the form from submitting
-//     var name = $('input[name="name"]').val();
-//     var email = $('input[name="email"]').val();
-//     var subject = $('input[name="subject"]').val();
-//     var message = $('textarea[name="message"]').val();
-//     var phone = $('input[name="phone"]').val();
-//     if (name == '' || email == '' || subject == '' || message == '' || phone == '') {
-//       alert('Please fill all fields!');
-//     } else {
-//       // send the data to Google Sheets using AJAX
-//       $.ajax({
-//         url: 'https://script.google.com/macros/s/AKfycbyLAOi1FMvbNB28vc8fU09MfHKcseHt_ziLwLScD0iLDNRxm29ejU4rv3pu7P8G2Bcd2w/exec',
-//         type: 'post',
-//         data: $('#contact-form').serialize(),
-//         success: function(response) {
-//           alert('Form submitted successfully!');
-//           $('#contact-form')[0].reset();
-//         },
-//         error: function(xhr, status, error) {
-//           alert('Form submission failed: ' + error);
-//         }
-//       });
-//     }
-//   });
-// });
-
-
 function validation(){
-  var name = document.getElementById('name').value
-  var email = document.getElementById('email').value
-  var message = document.getElementById('message').value
-  var nameValidation = /^[A-Za-z]+$/;
-  
+    var name = document.getElementById('name').value
+    var email = document.getElementById('email').value
+    var message = document.getElementById('message').value
+    var nameValidation = /^[a-zA-Z ]*$/;
+    reg = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    email_result = reg.test(email);
 
-  if(name == ''){
-    alert("Enter a valid name please");
-    return false
-  }else if(name.length<=3){
-    alert("The  name must be atleast 3 character")
-    // document.getElementById('name').innerHTML = " please enter a valid name"
-    return false
-  } else if(name.indexOf(" ") >= 0){
-    alert("Name not entered.")
-    return false
-  } else if (!nameValidation.test(name) ) {
-    alert("Invalid characters");
-    return false
-  }
-  else{
-    document.getElementById('name').innerHTML=""
-    name = true
-  }
+    if(name == ''){
+      alert("Enter a valid name please");
+      return false
+    }else if(name.length<=3){
+      alert("The name must be atleast 3 character")
+      return false
+    }  else if (!nameValidation.test(name) ) {
+      alert("Invalid characters");
+      return false
+    }
+    else{
+      document.getElementById('name').innerHTML=""
+      name = true
+    }
 
- 
-  if(email == ''){
-    // document.getElementById('email').innerHTML = "please enter your email"
-    alert("Please enter your correct email")
-    return false
-  }else if(email.length<10){
-    // document.getElementById('email').innerHTML = " please enter a valid email"
-    alert("Please enter  your correct E-mail")
-    return false
-  }
-  else{
-    document.getElementById('email').innerHTML = ""
-    email = true
-  }
+   
+    if(email == ''){
+      alert("Please enter your correct email")
+      return false
+    }else if(email.length<8){
+      alert("Please enter your correct E-mail")
+      return false
+    } else if(!email_result){
+      alert("Please provide a correct E-mail")
+      return false
+    }
+    else{
+      document.getElementById('email').innerHTML = ""
+      email = true
+    }
 
+    if(message == ''){
+      alert("Message must not be Blank")
+      return false
+    }else if(message.length<10){
+      alert("Message must be more than 10 char")
+      return false
+    }
+    else{
+      document.getElementById('message').innerHTML = ""
+      message = true
+    }
 
-  if(message == ''){
-    // document.getElementById('message_error').innerHTML = "please enter any message"
-    alert("Message must not be Blank")
-    return false
-  }else if(message.length<5){
-    // document.getElementById('message_error').innerHTML = " please enter atleast 10 characters"
-    alert("Message must be more than 10  char")
-    return false
-  }
-  else{
-    document.getElementById('message_error').innerHTML = ""
-    message = true
-  }
+    return true
 }
 
+      $("#name").on("keyup", function(){
+        let name = $(this).val();
+        $("#name").val(name.trimStart())
+      });
 
 
-  $("#contact-form").submit((e)=>{
-      e.preventDefault()
-      $.ajax({
-          url:"https://script.google.com/macros/s/AKfycbyLAOi1FMvbNB28vc8fU09MfHKcseHt_ziLwLScD0iLDNRxm29ejU4rv3pu7P8G2Bcd2w/exec",
-          data:$("#contact-form").serialize(),
-          method:"post",
-          success:function (response){
-              alert("Form submitted successfully")
-              window.location.reload()
-          },
-          error:function (err){
-              alert("Something Error")
+    $("#contact-form").submit((e)=>{
+      if(validation()){
+        e.preventDefault()
+        $.ajax({
+            url:"https://script.google.com/macros/s/AKfycbyEk9gUqoG2opANLBqkJTzwsEKp93WfabsfauDkhWUimxPzIMy-b2kqbdunNjE5mbGWdA/exec",
+            data:$("#contact-form").serialize(),//converts the form data into json
+            method:"post",
+            success:function (response){
+                alert("Form submitted successfully")
+                window.location.reload()
+            },
+            error:function (err){
+                alert("Something Error")
 
-          }
-      })
+            }
+        })
+      }
     })
